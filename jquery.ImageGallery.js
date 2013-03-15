9cd5eef288bb8d0454bbd026afbaab5dd4664371
Scott Stadt <sstadt@fullsail.edu>
2013-03-15 11:39:52 -0400

    Begun Code Integration
    Restructured component detection to be similar to Vertical-Slider
    plugin.

--- jquery.ImageGallery.js
+++ jquery.ImageGallery.js
@@ -94,221 +94,239 @@
 				
 				return false;
 			});
+		
+			*/
+		
+			// grab the stuff we're going to use
+			var gallery = $(this),
+				next = $('#' + gallery.attr('id') + '-next'),
+				prev = $('#' + gallery.attr('id') + '-prev'),
+				pos = $('#' + gallery.attr('id') + '-pos'),
+				thumbs = $('.thumbnail'),
+				viewer = $('#image_viewer'),
+				overlay = $('#modal_overlay'),
+				gallery_thumbs = 15,
+				newview,
+				alignImage = function(view){
+					var height = view.css('height'),
+						img = view.find('img'),
+						imgheight = img.get(0).height,
+						hpadding = (parseInt(height,10) - parseInt(imgheight,10)) / 2
+					;
+				
+					if (hpadding > 0 && imgheight > 0) {
+						img.css('margin-top',hpadding);
+					} else if (imgheight === 0) {
+						//console.log('image align error(' + img.attr('src').substr(img.attr('src').lastIndexOf('/')+1) + '): the image height has returned as 0');
+					}
+				}
+			;
+	
+			if (thumbs.length > 0) {
+	
+				// set up the thumbnails
+				thumbs.each(function(){
+					var that = $(this);
+					
+					if (that.parent().css('display') !== 'none') {
+						// align the image once it's loaded
+						that.load(function(){
+							
+							alignImage($(this));
+						});
+					}
+					
+					that.bind('click', function(){
+						
+						// grab the image location
+						var img = $(this).find('img').attr('src'),
+							fullimg = img.substr(0,img.indexOf('thumbs')) + img.substr(img.lastIndexOf('/')+1)
+						;
+						
+						// place the image in the viewer
+						viewer.empty();
+						viewer.append('<img src="'+ fullimg + '" />');
+						viewer.find('img').load(function(){
+							
+							// show the viewer
+							viewer.parent().fadeIn(easedur);
+							overlay.fadeIn(easedur);
+							alignImage(viewer);
+						});
+						
+					});
+		
+				});
+				
+				// set up thumbnail tabs
+				gallery.find('.tab').hide(0, function(){
+					gallery.find('.tab:first').show();
+				});
+				
+				// set up thumbnail tab navigation indicator
+				pos.text('page 1 of ' + Math.ceil(thumbs.length / gallery_thumbs))
+				
+				// set up the previous tab button
+				prev.bind('click',function(){
+					
+					// change the image page
+					gallery.children().each(function(){
+						var that = $(this);
+						
+						if (that.css('display') !== 'none') {
+							
+							that.fadeOut(options.duration, function(){
+		
+								// determine what the next view is
+								if (that.prev().attr('id') !== undefined) {
+									newview = that.prev();
+								} else {
+									newview = that.siblings(':last');
+								}
+		
+								// initialize the next view
+								pos.text('page ' + newview.attr('id').substr(7) + ' of ' + Math.ceil(thumbs.length / gallery_thumbs));
+								newview.find('img').each(function(){
+									$(this).load(function(){
+										alignImage($(this).parent());
+									});
+								});
+								newview.fadeIn(options.duration);
+							});
+							
+							return false;
+						}
+					});
+					
+					return false;
+				});
+				
+				// set up the next tab button
+				next.bind('click',function(){
+					
+					// change the image page
+					gallery.children().each(function(){
+						var that = $(this);
+						
+						if (that.css('display') !== 'none') {
+							
+							that.fadeOut(options.duration, function(){
+		
+								// determine what the next view is
+								if (that.next().attr('id') !== undefined) {
+									newview = that.next();
+								} else {
+									newview = that.siblings(':first');
+								}
+		
+								// initialize the next view
+								pos.text('page ' + newview.attr('id').substr(7) + ' of ' + Math.ceil(thumbs.length / gallery_thumbs));
+								newview.find('img').each(function(){
+									$(this).load(function(){
+										alignImage($(this).parent());
+									});
+								});
+								newview.fadeIn(options.duration);
+							});
+							
+							return false;
+						}
+					});
+					
+					return false;
+				});
+				
+				// set up viewer controls
+				viewer.parent().find('#viewer_next').bind('click',function(){
+					// viewer next button
+					
+					// get the next image
+					var viewerimg = viewer.find('img').attr('src'),
+						nextimg = viewerimg.substr(0,viewerimg.lastIndexOf('/')) + '/thumbs' + viewerimg.substr(viewerimg.lastIndexOf('/'));
+					
+					var index = 0;
+					
+					// find the next image and navigate to it
+					thumbs.each(function(){
+						var thumbimg = $(this).find('img').attr('src');
+		
+						if (thumbimg === nextimg) {
+							if (index + 1 > thumbs.length - 1) {
+								nextimg = $(thumbs[0]).find('img').attr('src');
+							} else {
+								nextimg = $(thumbs[index+1]).find('img').attr('src');
+							}
+							nextimg = nextimg.substr(0,nextimg.indexOf('thumbs')) + nextimg.substr(nextimg.lastIndexOf('/')+1);
+		
+							// place the image in the viewer
+							viewer.fadeOut(easedur, function(){
+								viewer.empty();
+								viewer.append('<img src="'+ nextimg + '" />');
+								viewer.find('img').load(function(){
+									viewer.fadeIn(easedur);
+									alignImage(viewer);
+								});
+							});
+							
+							
+							return false;
+						}
+						
+						index++;
+					});
+					
+					return false;
+				}).next().bind('click',function(){
+					// viewer previous button
+					
+					// get the next image
+					var viewerimg = viewer.find('img').attr('src'),
+						nextimg = viewerimg.substr(0,viewerimg.lastIndexOf('/')) + '/thumbs' + viewerimg.substr(viewerimg.lastIndexOf('/'));
+					
+					var index = 0;
+					
+					// find the next image and navigate to it
+					thumbs.each(function(){
+						var thumbimg = $(this).find('img').attr('src');
+		
+						if (thumbimg === nextimg) {
+							if (index === 0) {
+								nextimg = $(thumbs[thumbs.length-1]).find('img').attr('src');
+							} else {
+								nextimg = $(thumbs[index-1]).find('img').attr('src');
+							}
+							nextimg = nextimg.substr(0,nextimg.indexOf('thumbs')) + nextimg.substr(nextimg.lastIndexOf('/')+1);
+		
+							// place the image in the viewer
+							viewer.fadeOut(easedur, function(){
+								viewer.empty();
+								viewer.append('<img src="'+ nextimg + '" />');
+								viewer.find('img').load(function(){
+									viewer.fadeIn(easedur);
+									alignImage(viewer);
+								});
+							});
+							
+							
+							return false;
+						}
+						
+						index++;
+					});
+					
+					return false;
+				}).next().bind('click',function(){
+					// viewer close button
+		
+					// hide the viewer
+					viewer.parent().fadeOut(easedur);
+					overlay.fadeOut(easedur);
+				
+					return false;
+				});
+			}
 			
 		});
-		
-		*/
-		
-		// grab the stuff we're going to use
-		var thumbs = $('.thumbnail'),
-			viewer = $('#image_viewer'),
-			overlay = $('#modal_overlay'),
-			img_content = $('#tabs'),
-			gallery_thumbs = 15,
-			newview
-		;
-
-		if (thumbs.length > 0) {
-
-			// set up the thumbnails
-			thumbs.each(function(){
-				var that = $(this);
-				
-				if (that.parent().css('display') !== 'none') {
-					// align the image once it's loaded
-					that.imagesLoaded(function(){
-						
-						alignImage($(this));
-					});
-				}
-				
-				that.bind('click', function(){
-					
-					// grab the image location
-					var img = $(this).find('img').attr('src'),
-						fullimg = img.substr(0,img.indexOf('thumbs')) + img.substr(img.lastIndexOf('/')+1)
-					;
-					
-					// place the image in the viewer
-					viewer.empty();
-					viewer.append('<img src="'+ fullimg + '" />');
-					viewer.find('img').imagesLoaded(function(){
-						
-						// show the viewer
-						viewer.parent().fadeIn(easedur);
-						overlay.fadeIn(easedur);
-						alignImage(viewer);
-					});
-					
-				});
-	
-			});
-			
-			// set up thumbnail tabs
-			img_content.find('.tab').hide(0, function(){
-				img_content.find('#img_tab1').show();
-			});
-			
-			// set up thumbnail navigation
-			img_content.prev().find('p').text('page 1 of ' + Math.ceil(thumbs.length / gallery_thumbs)).prev().bind('click',function(){
-				// previous image button
-				
-				// change the image page
-				img_content.children().each(function(){
-					var that = $(this);
-					
-					if (that.css('display') !== 'none') {
-						
-						that.fadeOut(easedur, function(){
-	
-							// determine what the next view is
-							if (that.prev().attr('id') !== undefined) {
-								newview = that.prev();
-							} else {
-								newview = that.siblings(':last');
-							}
-	
-							// initialize the next view
-							nav_text = 'page ' + newview.attr('id').substr(7) + ' of ' + Math.ceil(thumbs.length / gallery_thumbs);
-							img_content.prev().find('p').text(nav_text);
-							newview.find('img').each(function(){
-								$(this).imagesLoaded(function(){
-									alignImage($(this).parent());
-								});
-							});
-							newview.fadeIn(easedur);
-						});
-						
-						return false;
-					}
-				});
-				
-				return false;
-			}).prev().bind('click',function(){
-				// next image button
-				
-				// change the image page
-				img_content.children().each(function(){
-					var that = $(this);
-					
-					if (that.css('display') !== 'none') {
-						
-						that.fadeOut(easedur, function(){
-	
-							// determine what the next view is
-							if (that.next().attr('id') !== undefined) {
-								newview = that.next();
-							} else {
-								newview = that.siblings(':first');
-							}
-	
-							// initialize the next view
-							nav_text = 'page ' + newview.attr('id').substr(7) + ' of ' + Math.ceil(thumbs.length / gallery_thumbs);
-							img_content.prev().find('p').text(nav_text);
-							newview.find('img').each(function(){
-								$(this).imagesLoaded(function(){
-									alignImage($(this).parent());
-								});
-							});
-							newview.fadeIn(easedur);
-						});
-						
-						return false;
-					}
-				});
-				
-				return false;
-			});
-			
-			// set up viewer controls
-			viewer.parent().find('#viewer_next').bind('click',function(){
-				// viewer next button
-				
-				// get the next image
-				var viewerimg = viewer.find('img').attr('src'),
-					nextimg = viewerimg.substr(0,viewerimg.lastIndexOf('/')) + '/thumbs' + viewerimg.substr(viewerimg.lastIndexOf('/'));
-				
-				var index = 0;
-				
-				// find the next image and navigate to it
-				thumbs.each(function(){
-					var thumbimg = $(this).find('img').attr('src');
-	
-					if (thumbimg === nextimg) {
-						if (index + 1 > thumbs.length - 1) {
-							nextimg = $(thumbs[0]).find('img').attr('src');
-						} else {
-							nextimg = $(thumbs[index+1]).find('img').attr('src');
-						}
-						nextimg = nextimg.substr(0,nextimg.indexOf('thumbs')) + nextimg.substr(nextimg.lastIndexOf('/')+1);
-	
-						// place the image in the viewer
-						viewer.fadeOut(easedur, function(){
-							viewer.empty();
-							viewer.append('<img src="'+ nextimg + '" />');
-							viewer.find('img').imagesLoaded(function(){
-								viewer.fadeIn(easedur);
-								alignImage(viewer);
-							});
-						});
-						
-						
-						return false;
-					}
-					
-					index++;
-				});
-				
-				return false;
-			}).next().bind('click',function(){
-				// viewer previous button
-				
-				// get the next image
-				var viewerimg = viewer.find('img').attr('src'),
-					nextimg = viewerimg.substr(0,viewerimg.lastIndexOf('/')) + '/thumbs' + viewerimg.substr(viewerimg.lastIndexOf('/'));
-				
-				var index = 0;
-				
-				// find the next image and navigate to it
-				thumbs.each(function(){
-					var thumbimg = $(this).find('img').attr('src');
-	
-					if (thumbimg === nextimg) {
-						if (index === 0) {
-							nextimg = $(thumbs[thumbs.length-1]).find('img').attr('src');
-						} else {
-							nextimg = $(thumbs[index-1]).find('img').attr('src');
-						}
-						nextimg = nextimg.substr(0,nextimg.indexOf('thumbs')) + nextimg.substr(nextimg.lastIndexOf('/')+1);
-	
-						// place the image in the viewer
-						viewer.fadeOut(easedur, function(){
-							viewer.empty();
-							viewer.append('<img src="'+ nextimg + '" />');
-							viewer.find('img').imagesLoaded(function(){
-								viewer.fadeIn(easedur);
-								alignImage(viewer);
-							});
-						});
-						
-						
-						return false;
-					}
-					
-					index++;
-				});
-				
-				return false;
-			}).next().bind('click',function(){
-				// viewer close button
-	
-				// hide the viewer
-				viewer.parent().fadeOut(easedur);
-				overlay.fadeOut(easedur);
-				
-				return false;
-			});
-		}
 			
 	};
 		
